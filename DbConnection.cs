ad287973468b07c10f14185529b2adbd99a4d864
anantharaman.s <anantharaman.s@gofrugal.com>
2023-09-05 12:03:12 +0530

    first commit

---
+++ DbConnection.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace App1
+{
+
+    public class DatabaseConnection
+    {
+        private static SqlConnection _connection = null;
+        private static readonly object _lock = new object();
+
+        public static SqlConnection GetConnection()
+        {
+            if (_connection == null)
+            {
+                lock (_lock)
+                {
+                    if (_connection == null)
+                    {
+                        string connectionString = "Data Source=ANANTH-18788\\SQLEXPRESS;Initial Catalog=\"Company DB\";Integrated Security=True";
+                        _connection = new SqlConnection(connectionString);
+                        _connection.Open();
+                    }
+                }
+            }
+            return _connection;
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace App1
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+
+            SqlConnection connection = DatabaseConnection.GetConnection();
+
+            string sqlQuery = "SELECT Name, * FROM Employee_Detail";
+            List<string> strings = new List<string>();
+            using (SqlCommand command = new SqlCommand(sqlQuery, connection))
+            {
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        string columnName = reader.GetString(reader.GetOrdinal("Name"));
+                        strings.Add(columnName);
+                    }
+                }
+            }
+
+        }
+
+        private void myButton_Click(object sender, RoutedEventArgs e)
+        {
+            myButton.Content = "Clicked";
+        }
+    }
+}
